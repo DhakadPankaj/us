017925c4dd49405329830228bdf9cb54848d47e9
Pankaj Dhakad <63622417+DhakadPankaj@users.noreply.github.com>
2026-02-04 18:46:08 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const img1 = "./img/lego-1.png";
+
+const img2 = "./img/lego-2.png";
+
+const img3 = "./img/lego-3.png";
+
+const img4 = "./img/lego-4.png";
+
+const img5 = "./img/lego-5.png";
+
+
+// define your images here
+const images = [ img1,img2, img3, img4, img5];
+
+// this chooses a random number from all available image indices
+var randomImage = images[Math.floor(Math.random() * images.length)];
+// make the URL into a proper image tag
+var image = "<img src='" + randomImage + "'>";
+// append to the div
+document.getElementById("valentines-container").innerHTML = image;
+
+const refreshButton = document.getElementById('refreshButton');
+
+refreshButton.addEventListener('click', function() {
+  location.reload();
+});
